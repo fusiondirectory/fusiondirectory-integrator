a92dcf14e8224675630a873956df746a7f7bb767
Thibault Dockx <thibault.dockx@fusiondirectory.org>
2024-08-26 17:15:21 +0100

    :ambulance: phpcs

--- src/FusionDirectory/Audit/AuditLib.php
+++ src/FusionDirectory/Audit/AuditLib.php
@@ -21,9 +21,9 @@ class AuditLib
   public function __construct (
     int $auditRetention,
     array $auditList,
-    ?object $gateway = NULL,
-    ?string $subTaskDN = NULL,
-    ?string $subTaskCN = NULL,
+    ?object $gateway     = NULL,
+    ?string $subTaskDN   = NULL,
+    ?string $subTaskCN   = NULL,
     ?Ldap\Link $ldapBind = NULL
   )
   {
@@ -123,8 +123,7 @@ class AuditLib
    * @throws Exception
    * Note : Simply take a generalized Ldap time (with UTC = Z) and transform it to php object dateTime.
    */
-  public
-  function generalizeLdapTimeToPhpObject ($generalizeLdapDateTime)
+  public function generalizeLdapTimeToPhpObject ($generalizeLdapDateTime)
   {
     // Extract the date part (first 8 characters: YYYYMMDD), we do not care about hour and seconds.
     $auditTimeFormatted = substr($generalizeLdapDateTime, 0, 8);
